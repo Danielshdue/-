24364906474840582583811b99c071ad6faad995
Danielshdue <petrov.daniel0@gmail.com>
2025-04-24 11:22:32 +0300

    Add files via upload

---
+++ 5ta zadacha.cpp
@@ -0,0 +1,37 @@
+﻿// 5ta zadacha.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+    //masiv1 i masiv2 za koito masiv2[i]=masiv1[i]*i
+    const int n = 8;
+    int a[n];
+    int b[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf_s("%d", &a[i]);
+        b[i] = a[i] * i;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", b[i]);
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
