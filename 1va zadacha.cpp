092803699cf17d47050445ac75b59df36b324993
Danielshdue <petrov.daniel0@gmail.com>
2025-04-24 11:32:20 +0300

    Add files via upload

---
+++ 1va zadacha.cpp
@@ -0,0 +1,34 @@
+﻿// 1va zadacha.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+    //10 celi chisla v praw i obraten red
+    int masiv[10];
+    for (int i = 0; i < 10; i++)
+    {
+        scanf_s("%d", &masiv[i]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", masiv[i]);
+    }
+    printf("\n");
+    for (int i = 9; i >=0; i--)
+    {
+        printf("%d ", masiv[i]);
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ 2ra zadacha.cpp
@@ -0,0 +1,34 @@
+﻿// 2ra zadacha.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+    //suma i sredno aritmetichno na 10 chisla tip double
+    double masiv[10];
+    double sum=0;
+    double sredno = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        scanf_s("%lf", &masiv[i]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        sum += masiv[i];
+    }
+    sredno = sum / 10;
+    printf("The sum equals %lf\n", sum);
+    printf("The average equals %lf", sredno);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
